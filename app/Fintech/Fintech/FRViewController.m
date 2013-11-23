7c22c5f2c5279538c1270d959a265621d0607ba6
AlexTorres <jalexandert@gmail.com>
2013-11-23 01:33:23 -0500

    ios project created.

---
+++ app/Fintech/Fintech/FRViewController.m
@@ -0,0 +1,29 @@
+//
+//  FRViewController.m
+//  Fintech
+//
+//  Created by John A Torres B on 23/11/13.
+//  Copyright (c) 2013 adminova. All rights reserved.
+//
+
+#import "FRViewController.h"
+
+@interface FRViewController ()
+
+@end
+
+@implementation FRViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ app/Fintech/Fintech/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Fintech
+//
+//  Created by John A Torres B on 23/11/13.
+//  Copyright (c) 2013 adminova. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "FRAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([FRAppDelegate class]));
+    }
+}
